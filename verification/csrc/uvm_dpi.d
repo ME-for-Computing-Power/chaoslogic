2cc98c11b9934df36d0fc8e408a31e543cc258f2
zhanyue191 <974695111@qq.com>
2025-06-07 20:22:24 +0800

    fix verification

---
+++ verification/csrc/uvm_dpi.d
@@ -0,0 +1,5 @@
+uvm_dpi.o: /home/zhanyue/uvm/uvm-1.1a/src/dpi/uvm_dpi.cc \
+ /home/zhanyue/uvm/uvm-1.1a/src/dpi/uvm_regex.cc \
+ /eda/vcs/include/vpi_user.h /home/zhanyue/uvm/uvm-1.1a/src/dpi/uvm_hdl.c \
+ /eda/vcs/include/veriuser.h /eda/vcs/include/svdpi.h \
+ /home/zhanyue/uvm/uvm-1.1a/src/dpi/uvm_svcmd_dpi.c
